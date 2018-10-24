f9e9c68a72cfea37d69d2421cfe8aacf8ca2176d
Sergiu221 <sergiu221@gmail.com>
2018-10-24 19:29:40 +0200

    feat: PROIECT-3added cross origin annotation.

--- src/main/java/com/sergiu/controller/HelloController.java
+++ src/main/java/com/sergiu/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.sergiu.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sergiu.model.Hello;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class HelloController {
 
