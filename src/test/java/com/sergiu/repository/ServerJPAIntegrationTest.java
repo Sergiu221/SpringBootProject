b0068510ec1c5f4f737abba4413d14bb61438459
Sergiu221 <sergiu221@gmail.com>
2018-10-27 12:04:08 +0200

    fix : PROIECT-16 deleted anything realeated to "Hello World!"

--- src/test/java/com/sergiu/repository/ServerJPAIntegrationTest.java
+++ src/test/java/com/sergiu/repository/ServerJPAIntegrationTest.java
@@ -12,28 +12,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.sergiu.Application;
-import com.sergiu.entity.HelloEntity;
 import com.sergiu.entity.TeacherEntity;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class ServerJPAIntegrationTest {
 	@Autowired
-	private HelloRepository helloRepository;
-
-	@Autowired
 	private TeacherRepository teacherRepository;
-
-	@Test
-	public void givenGenericEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
-		HelloEntity entity = new HelloEntity();
-		entity.setId(1);
-		entity.setMessage("Salut Sergiu!");
-		HelloEntity genericEntity = helloRepository.save(entity);
-		Optional<HelloEntity> foundEntity = helloRepository.findById(genericEntity.getId());
-		assertNotNull(foundEntity);
-		assertEquals(genericEntity.getMessage(), foundEntity.get().getMessage());
-	}
 
 	@Test
 	public void givenTeacherEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
