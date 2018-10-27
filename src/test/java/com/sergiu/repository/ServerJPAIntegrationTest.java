6a95ed98c6f72b47efe323335cce6e4b22160489
Sergiu221 <sergiu221@gmail.com>
2018-10-27 11:37:29 +0200

    test : PROIECT-12 added test on teacher table.

--- src/test/java/com/sergiu/repository/ServerJPAIntegrationTest.java
+++ src/test/java/com/sergiu/repository/ServerJPAIntegrationTest.java
@@ -13,22 +13,39 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.sergiu.Application;
 import com.sergiu.entity.HelloEntity;
+import com.sergiu.entity.TeacherEntity;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class ServerJPAIntegrationTest {
-	 @Autowired
-	    private HelloRepository helloRepository;
-	 
-	    @Test
-	    public void givenGenericEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
-	    	HelloEntity entity=new HelloEntity();
-	    	entity.setId(1);
-	    	entity.setMessage("Salut Sergiu!");
-	    	HelloEntity genericEntity = helloRepository
-	          .save(entity);
-	    	Optional<HelloEntity> foundEntity = helloRepository.findById(genericEntity.getId());
-	        assertNotNull(foundEntity);
-	        assertEquals(genericEntity.getMessage(), foundEntity.get().getMessage());
-	    }
+	@Autowired
+	private HelloRepository helloRepository;
+
+	@Autowired
+	private TeacherRepository teacherRepository;
+
+	@Test
+	public void givenGenericEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
+		HelloEntity entity = new HelloEntity();
+		entity.setId(1);
+		entity.setMessage("Salut Sergiu!");
+		HelloEntity genericEntity = helloRepository.save(entity);
+		Optional<HelloEntity> foundEntity = helloRepository.findById(genericEntity.getId());
+		assertNotNull(foundEntity);
+		assertEquals(genericEntity.getMessage(), foundEntity.get().getMessage());
+	}
+
+	@Test
+	public void givenTeacherEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
+		TeacherEntity entity = new TeacherEntity();
+		entity.setId(1);
+		entity.setFirstName("firtsName");
+		entity.setMiddleName("middleName");
+		entity.setLastName("lastName");
+		TeacherEntity expected = teacherRepository.save(entity);
+		Optional<TeacherEntity> foundEntity = teacherRepository.findById(expected.getId());
+		assertNotNull(foundEntity);
+		assertEquals(expected, foundEntity.get());
+		teacherRepository.delete(foundEntity.get());
+	}
 }
