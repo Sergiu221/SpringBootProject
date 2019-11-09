b05f192486e7873afc15a49cdb4e972044f56634
Sergiu221 <sergiu221@gmail.com>
2019-11-09 15:21:28 +0100

    Clean pom.xml

--- src/test/java/com/sergiu/repository/CandidatesRepositoryIntegrationTest.java
+++ src/test/java/com/sergiu/repository/CandidatesRepositoryIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,6 +17,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @ActiveProfiles("test")
+@SpringBootTest(classes = CandidateRepository.class)
 public class CandidatesRepositoryIT {
 
     @Autowired
--- src/test/java/com/sergiu/repository/SupervisorsRepositoryIntegrationTest.java
+++ src/test/java/com/sergiu/repository/SupervisorsRepositoryIntegrationTest.java
@@ -1,13 +1,14 @@
 package com.sergiu.repository;
 
-import com.sergiu.entity.CandidateEntity;
-import com.sergiu.entity.HallEntity;
+import com.sergiu.entity.SupervisorEntity;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,42 +17,47 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @ActiveProfiles("test")
-public class HallsRepositoryIT {
+@SpringBootTest(classes = SupervisorRepository.class)
+public class SupervisorsRepositoryIT {
 
     @Autowired
     private TestEntityManager entityManager;
 
     @Autowired
-    private HallRepository hallRepository;
+    private SupervisorRepository supervisorRepository;
 
     @Before
     public void init() {
-        HallEntity entity = new HallEntity();
-        entity.setName("Sala1");
-        entity.setSize(60);
-        entity.setUtilizableSize(30);
+        SupervisorEntity entity = new SupervisorEntity();
 
+        entity.setFirstName("Supervisor Name1");
+        entity.setLastName("Supervisor LastName1");
         entityManager.persist(entity);
 
-        HallEntity entity2 = new HallEntity();
-        entity2.setName("Sala2");
-        entity2.setSize(52);
-        entity2.setUtilizableSize(25);
+        SupervisorEntity entity2 = new SupervisorEntity();
 
+        entity.setFirstName("Supervisor Name2");
+        entity.setLastName("Supervisor LastName2");
         entityManager.persist(entity2);
+        entityManager.flush();
+    }
+
+    @After
+    public void clear() {
+        entityManager.clear();
         entityManager.flush();
     }
 
     @Test
     public void testFindByIdOnHallsRepository() {
-        HallEntity found = hallRepository.findById(1).get();
+        SupervisorEntity found = supervisorRepository.findById(1).get();
         assertEquals(found.getId(), 1);
     }
 
     @Test
     public void testDeleteByIdOnHallsRepository() {
-        int totalNrOfHalls = hallRepository.findAll().size();
-        hallRepository.deleteById(3);
-        assertEquals(totalNrOfHalls - 1, hallRepository.findAll().size());
+        int totalNrOfHalls = supervisorRepository.findAll().size();
+        supervisorRepository.deleteById(3);
+        assertEquals(totalNrOfHalls - 1, supervisorRepository.findAll().size());
     }
 }
