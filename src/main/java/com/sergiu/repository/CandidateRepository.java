9beb2f381e8190314c80e4e8c5a6f97ddedb3298
Sergiu221 <sergiu221@gmail.com>
2019-11-09 13:29:29 +0100

    Add new method in CandidatesRepositorY

--- src/main/java/com/sergiu/repository/CandidateRepository.java
+++ src/main/java/com/sergiu/repository/CandidateRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sergiu.entity.CandidateEntity;
 
-public interface CandidateRepository extends JpaRepository<CandidateEntity, Integer> {
+public interface CandidateRepository extends JpaRepository<CandidateEntity, Long> {
 
+    CandidateEntity findByCnp(Long cnp);
+
+    void deleteByCnp(Long cnp);
 }
