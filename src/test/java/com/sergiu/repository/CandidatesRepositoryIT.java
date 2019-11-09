7e9b6a2ff1f78cf884cb49a946a39621e892f49a
Sergiu221 <sergiu221@gmail.com>
2019-11-09 13:28:45 +0100

    Provide correct IT's for candidates using h2database

---
+++ src/test/java/com/sergiu/repository/CandidatesRepositoryIT.java
@@ -0,0 +1,58 @@
+package com.sergiu.repository;
+
+import com.sergiu.entity.CandidateEntity;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ActiveProfiles("test")
+public class CandidatesRepositoryIT {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Before
+    public void init() {
+        CandidateEntity entity = new CandidateEntity();
+        entity.setCnp(1940122374500L);
+        entity.setFirstName("Sergiu-Adrian");
+        entity.setLastName("Volocaru");
+        entity.setHighSchool("Liceul Teoretic Emil Racovita");
+
+        entityManager.persist(entity);
+
+        CandidateEntity entity2 = new CandidateEntity();
+        entity2.setCnp(1940122374502L);
+        entity2.setFirstName("Sergiu-Constatin");
+        entity2.setLastName("Volocaru");
+        entity2.setHighSchool("Liceul Teoretic Emil Racovita");
+
+        entityManager.persist(entity2);
+        entityManager.flush();
+    }
+
+    @Test
+    public void testFindByCnpOnCandidateRepository() {
+        CandidateEntity found = candidateRepository.findByCnp(1940122374500L);
+        assertEquals(found.getCnp(), Long.valueOf(1940122374500L));
+    }
+
+    @Test
+    public void testDeleteOnCandidateRepository() {
+        int totalNrOfCandidates = candidateRepository.findAll().size();
+        candidateRepository.deleteByCnp(1940122374500L);
+        assertEquals(totalNrOfCandidates - 1, candidateRepository.findAll().size());
+    }
+}
--- src/test/java/com/sergiu/repository/ServerJPAIntegrationTest.java
+++ src/test/java/com/sergiu/repository/ServerJPAIntegrationTest.java
@@ -20,59 +20,40 @@ import com.sergiu.entity.SupervisorEntity;
 @SpringBootTest(classes = Application.class)
 public class ServerJPAIntegrationTest {
 
-	@Autowired
-	private SupervisorRepository supervisorRepository;
+    @Autowired
+    private SupervisorRepository supervisorRepository;
 
-	@Autowired
-	private HallRepository hallRepository;
+    @Autowired
+    private HallRepository hallRepository;
 
-	@Autowired
-	private CandidateRepository candidateRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
-	@Autowired
-	private CategoryRepository categoryRepository;
+    @Test
+    public void givenSupervisorEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
+        SupervisorEntity entity = new SupervisorEntity();
+        entity.setId(1);
+        entity.setFirstName("firtsName");
+        entity.setMiddleName("middleName");
+        entity.setLastName("lastName");
+        SupervisorEntity expected = supervisorRepository.save(entity);
+        Optional<SupervisorEntity> foundEntity = supervisorRepository.findById(expected.getId());
+        assertNotNull(foundEntity);
+        assertEquals(expected, foundEntity.get());
+        supervisorRepository.delete(foundEntity.get());
+    }
 
-	@Test
-	public void givenSupervisorEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
-		SupervisorEntity entity = new SupervisorEntity();
-		entity.setId(1);
-		entity.setFirstName("firtsName");
-		entity.setMiddleName("middleName");
-		entity.setLastName("lastName");
-		SupervisorEntity expected = supervisorRepository.save(entity);
-		Optional<SupervisorEntity> foundEntity = supervisorRepository.findById(expected.getId());
-		assertNotNull(foundEntity);
-		assertEquals(expected, foundEntity.get());
-		supervisorRepository.delete(foundEntity.get());
-	}
-
-	@Test
-	public void givenHallEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
-		HallEntity entity = new HallEntity();
-		entity.setId(2);
-		entity.setName("C308");
-		entity.setUtilizableSize(25);
-		entity.setSize(50);
-		HallEntity expected = hallRepository.save(entity);
-		Optional<HallEntity> foundEntity = hallRepository.findById(expected.getId());
-		assertNotNull(foundEntity);
-		assertEquals(expected, foundEntity.get());
-		hallRepository.delete(foundEntity.get());
-	}
-
-	@Test
-	public void givenCandidateEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
-		CandidateEntity entity = new CandidateEntity();
-		entity.setId(3);
-		entity.setCnp("1940122374514");
-		entity.setFirstName("Test First Name");
-		entity.setLastName("Test Last Name");
-		entity.setHighSchool("Liceul Teoretic Emil Racovita");
-		entity.setCategory(categoryRepository.findById("MRA").get());
-		CandidateEntity expected = candidateRepository.save(entity);
-		Optional<CandidateEntity> foundEntity = candidateRepository.findById(expected.getId());
-		assertNotNull(foundEntity);
-		assertEquals(expected, foundEntity.get());
-		candidateRepository.delete(foundEntity.get());
-	}
+    @Test
+    public void givenHallEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
+        HallEntity entity = new HallEntity();
+        entity.setId(2);
+        entity.setName("C308");
+        entity.setUtilizableSize(25);
+        entity.setSize(50);
+        HallEntity expected = hallRepository.save(entity);
+        Optional<HallEntity> foundEntity = hallRepository.findById(expected.getId());
+        assertNotNull(foundEntity);
+        assertEquals(expected, foundEntity.get());
+        hallRepository.delete(foundEntity.get());
+    }
 }
