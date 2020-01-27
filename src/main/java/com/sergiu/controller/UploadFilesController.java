d229fbbbc7baa4385c70b1715d6f60482a41d6b0
Sergiu221 <sergiu221@gmail.com>
2020-01-27 22:47:42 +0100

    Add state to database

--- src/main/java/com/sergiu/controller/UploadFilesController.java
+++ src/main/java/com/sergiu/controller/UploadFilesController.java
@@ -4,34 +4,41 @@ import com.sergiu.service.FilesService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+@CrossOrigin
 @RestController
 public class UploadFilesController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UploadFilesController.class);
 
     private FilesService filesService;
 
+    @Value("classpath:data-base/resources.ods")
+    private File resourcesDadaBase;
+
     @Autowired
     public UploadFilesController(FilesService filesService) {
         this.filesService = filesService;
     }
 
-
+    //@RequestParam("file") MultipartFile multipartFile
     @PostMapping(path = "files/load_database")
-    public ResponseEntity upLoadFileSetupDataBase(@RequestParam("file") MultipartFile multipartFile) throws Exception {
+    public ResponseEntity upLoadFileSetupDataBase() throws Exception {
 
         try {
-            LOGGER.info("File [{}] is loaded.\n", multipartFile.getResource().getFilename());
-            filesService.readAndInsertMainResources(multipartFile.getInputStream());
+            LOGGER.info("File [{}] is loaded.\n", resourcesDadaBase.getName());
+            filesService.readAndInsertMainResources(new FileInputStream(resourcesDadaBase));
         } catch (Exception e) {
             LOGGER.error(e.getMessage());
-            LOGGER.error("File [{}] fail to load.", multipartFile.getResource().getFilename());
+            LOGGER.error("File [{}] fail to load.", resourcesDadaBase.getName());
             throw e;
 
         }
@@ -40,15 +47,16 @@ public class UploadFilesController {
                 .body("File was successfully loaded");
     }
 
+    //@RequestParam("file") MultipartFile multipartFile
     @PostMapping(path = "files/load_grades")
-    public ResponseEntity<String> uploadGradesIntoDataBase(@RequestParam("file") MultipartFile multipartFile) throws Exception {
+    public ResponseEntity<String> uploadGradesIntoDataBase() throws Exception {
 
         try {
-            LOGGER.info("File [{}] with grads is loaded.\n", multipartFile.getResource().getFilename());
-            filesService.readAndInsertGradesIntoDataBase(multipartFile.getInputStream());
+            LOGGER.info("File [{}] with grads is loaded.\n", resourcesDadaBase.getName());
+            filesService.readAndInsertGradesIntoDataBase(new FileInputStream(resourcesDadaBase));
         } catch (Exception e) {
             LOGGER.error(e.getMessage());
-            LOGGER.error("File [{}] with grades fail to load.", multipartFile.getResource().getFilename());
+            LOGGER.error("File [{}] with grades fail to load.", resourcesDadaBase.getName());
             throw e;
 
         }
---
+++ src/main/java/com/sergiu/database/DbInit.java
@@ -0,0 +1,24 @@
+package com.sergiu.database;
+
+import com.sergiu.repository.ApplicationStateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DbInit implements CommandLineRunner {
+
+    @Autowired
+    private ApplicationStateRepository applicationStateRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        ApplicationState applicationState = new ApplicationState();
+        applicationState.setIsImportedResources("false");
+        applicationState.setIsDistributedFinalized("false");
+        applicationState.setIsDistributed("false");
+        applicationState.setIsExamFinish("false");
+        applicationStateRepository.save(applicationState);
+
+    }
+}
